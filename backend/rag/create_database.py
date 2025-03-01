06be663e14a0e6f557fd75b2f266c14735a63563
millenniumActress <166165886+millenniumActress@users.noreply.github.com>
2025-03-01 12:51:44 -0500

    Made more functions

--- backend/rag/create_database.py
+++ backend/rag/create_database.py
@@ -1,6 +1,6 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 import ollama
 import os
@@ -11,13 +11,21 @@ data_path = "books/pdf"
 
 def generate_data_store():
     documents = load_documents()
-    
-# Splitting text here
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000,
-    chunk_overlap = 500,
-    add_start_index = True,
-        )
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
 
-chunks = text_splitter.split_documents(documents)
+def load_documents():
+    loader = DirectoryLoader(data_path, glob='*.md')
+    documents = loader.load()
+    return documents
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 500,
+        length_function = len,
+        add_start_index = True,
+        )
+    chunks = text_splitter.split_documents(documents)
+
 
