555a67c71eb24941ddc54731e6c3b5d37d4287fc
millenniumActress <166165886+millenniumActress@users.noreply.github.com>
2025-03-01 12:28:00 -0500

    Made some changes in database file

--- backend/rag/create_database.py
+++ backend/rag/create_database.py
@@ -1,5 +1,16 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+import ollama
+import os
+import shutil
 
+chroma_path = "chroma"
+data_path = "books/pdf"
+
+def main():
+    
 # Splitting text here
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1000,
@@ -8,3 +19,4 @@ text_splitter = RecursiveCharacterTextSplitter(
         )
 
 chunks = text_splitter.split_documents(documents)
+
