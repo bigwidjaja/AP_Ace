f119a180f04ed3ac3f725277e3a0410b50375011
millenniumActress <166165886+millenniumActress@users.noreply.github.com>
2025-03-01 10:42:18 -0500

    Chat model works at a basic level

--- backend/chat_models/chat_model.py
+++ backend/chat_models/chat_model.py
@@ -2,15 +2,17 @@ import requests
 import json
 
 url = "http://127.0.0.1:11434/api/generate"
-
+prompt = input()
 data = { 
         "model": "deepseek-r1:7b",
-        "prompt": "What is 3 + 3?",
+        "prompt": prompt,
         "stream": False }
 
 response = requests.post(url, json=data)
+response_json = response.json()
+output = response_json["response"].split("</think>")
+print(output[-1])
 
-print(response.json())
 
 
 
