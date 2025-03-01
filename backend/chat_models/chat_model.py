0a213b18fca298a3c146e9a29e7cd7f4eb6a9141
millenniumActress <166165886+millenniumActress@users.noreply.github.com>
2025-03-01 10:30:31 -0500

    Made a basic chat model that answers some prompt..?

--- backend/chat_models/chat_model.py
+++ backend/chat_models/chat_model.py
@@ -1,3 +1,16 @@
-from langchain_ollama import ChatOllama
+import requests
+import json
+
+url = "http://127.0.0.1:11434/api/generate"
+
+data = { 
+        "model": "deepseek-r1:7b",
+        "prompt": "What is 3 + 3?",
+        "stream": False }
+
+response = requests.post(url, json=data)
+
+print(response.json())
+
 
 
