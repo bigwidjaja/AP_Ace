1bd5b9c9afdb01c88c89d304927e5fb83ec441d5
rsw72 <rsw72@drexel.edu>
2025-03-01 16:55:14 -0500

    yes

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,21 @@
 
 <body class="bg-[url('/gradient.jpg')] bg-fixed bg-cover w-full m-0 p-0 bg-no-repeat">
      <p class='text-white'> Skibidi </p>
-</body>
+</body> 
+
+<!--top left-->
+<div class="relative">
+     <img
+         src="/acorn.png"
+         alt="Acorn Logo"
+         class="absolute top-[-35px] left-[40px] object-contain"
+         style="width: 60px; height: 60px; max-width: 100px; max-height: 100px;"
+     >  
+     <span class="text-[30px] text-red-700 font-bold font-instrument-serif ml-[100px] relative top-[-10px]">ACE</span>
+ </div> 
+
+ 
+<!-- -->
 
 <style global>
      body {
