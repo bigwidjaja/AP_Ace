217b3a2d5f73968d08e67ae15fb4c2ed9cac8313
CurryKing <AksharPatel0215@users.noreply.github.com.>
2025-03-01 15:11:06 -0500

    Uses prompt & relevance filter to find 5 best sources of info for prompt

---
+++ backend/rag/compare_embeddings.py
@@ -0,0 +1,6 @@
+import argparse
+# from dataclasses import dataclass
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
