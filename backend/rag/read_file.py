862eb7e959b63f986d4de9cb92ff64f30e7c1ba1
millenniumActress <166165886+millenniumActress@users.noreply.github.com>
2025-03-01 22:25:15 -0500

    Removed selenium

--- backend/rag/read_file.py
+++ backend/rag/read_file.py
@@ -1,30 +1,5 @@
 import fitz  
 from pdfminer.high_level import extract_text
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from webdriver_manager.chrome import ChromeDriverManager
-
-#configuring selenium (for overcoming 403 and SSLCertificate Errors)
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("start-maximized")
-chrome_options.add_argument("disable-infobars")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-
-#setting up webdriver
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-#disguise
-driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
 def extract_pdf_text(pdf_path):
     with fitz.open(pdf_path) as doc:
